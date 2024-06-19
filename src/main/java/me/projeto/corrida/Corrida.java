d3d378bacfd47fabd4c78437d3abb697ca3b7402
ViniciusMelz <viniciusgmelz@gmail.com>
2024-06-19 18:33:59 -0300

    first commit

---
+++ src/main/java/me/projeto/corrida/Corrida.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.projeto.corrida;
+
+import me.projeto.view.TelaPrincipal;
+
+/**
+ *
+ * @author vinic
+ */
+public class Corrida {
+
+    public static void main(String[] args) {
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        telaPrincipal.setVisible(true);
+    }
+}
